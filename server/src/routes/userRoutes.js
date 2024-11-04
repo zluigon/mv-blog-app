5f5b2bcd4d9219ebd25968a9f31147b40e19410a
zluigon <luis.nmn.gonzalez@gmail.com>
2024-11-04 14:24:49 -0500

    fixed ssr error

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -4,7 +4,7 @@ import { registerUser, loginUser } from "../controllers/userController.js";
 
 const userRouter = Router();
 
-userRouter.post("/", registerUser);
+userRouter.post("/create", registerUser);
 userRouter.post("/login", loginUser);
 
 export default userRouter;
